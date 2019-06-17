96097caa669c1d55ae3c914f24dd09c43b767788
RachellS <railavie@LambdaSr118sMBP.attlocal.net>
2019-06-17 00:42:46 -0400

    added imagethumbnail component info. Commented out unused component import and uses in App.js and HeaderContainer.js to test App

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
-import HeaderTitle from './HeaderTitle';
-import HeaderContent from './HeaderContent';
+//import HeaderTitle from './HeaderTitle';
+//import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => {
     return (
         <div className="header-container">
             <ImageThumbnail />
-			<HeaderTitle />
-            <HeaderContent />
+			{/* <HeaderTitle /> */}
+            {/* <HeaderContent /> */}
         </div>
 
     );
