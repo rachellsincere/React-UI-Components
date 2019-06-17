1fb8209fdf6c44fb8ab175d112089f1b2e4e1036
Rachell Sincere <rachellsincere@gmail.com>
2019-06-17 01:11:20 -0400

    Added Header Title. uncommented in headercontainer

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className="header-title">
+        <h1 className="title-text">Lambda School</h1>
+        <p className="handle-text">@LambdaSchool</p>
+        <p className="timestamp">&#xb7; 26 Jan</p>
+        </div>
+    );
+};
+
+export default HeaderTitle;
