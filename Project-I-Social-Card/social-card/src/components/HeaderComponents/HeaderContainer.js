0c8f01c94ebcc6710bb74673883fb40613504178
Rachell Sincere <rachellsincere@gmail.com>
2019-06-18 10:45:00 -0400

    deleted header content import from container since it was duplicate. Setup maininput and symbol bar containers for calculator. started actionbtn, numberbtn, and calcdisplay components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
-import HeaderContent from './HeaderContent';
 
 const HeaderContainer = () => {
     return (
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = props => {
+    return <h1>{props.display}</h1>;
+};
+
+export default CalculatorDisplay;
