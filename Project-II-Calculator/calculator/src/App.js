d21bab15fbcbd7220d62f177666105ef52c3c72c
Rachell Sincere <rachellsincere@gmail.com>
2019-06-19 03:11:47 -0400

    deleted commented out styles and imports

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import Main from './components/ButtonComponents/MainInputContainer';
-//import NumberButton from './components/ButtonComponents/NumberButton';
 
-/*NumberButton.defaultProps ={ 
-  buttonStyle: 'white'
-
-   }; */
 
 const App = () => {
   return (
     <div className="app-container">
       <CalculatorDisplay displayStyle="display" text="0"/>
       <Main />
-      {/* <NumberButton /> */}
+      
    
    
     </div>
