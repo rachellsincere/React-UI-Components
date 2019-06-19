906e2471fa0340cd2a07972e288ebd4e9cc93ecd
Rachell Sincere <rachellsincere@gmail.com>
2019-06-19 02:46:07 -0400

    added components to maininput container without borders

--- Project-II-Calculator/calculator/src/components/ButtonComponents/MainInputContainer.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/MainInputContainer.js
@@ -7,6 +7,20 @@ const Main = () => {
         <div className="main-container">
             <Button buttonStyle="action-btn" text="clear" />
             <Button buttonStyle="function-btn" text="&#xf7;" />
+            <Button buttonStyle="number-btn" text="7" />
+            <Button buttonStyle="number-btn" text="8" />
+            <Button buttonStyle="number-btn" text="9" />
+            <Button buttonStyle="function-btn" text="&#xd7;" />
+            <Button buttonStyle="number-btn" text="4" />
+            <Button buttonStyle="number-btn" text="5" />
+            <Button buttonStyle="number-btn" text="6" />
+            <Button buttonStyle="function-btn" text="-" />
+            <Button buttonStyle="number-btn" text="1" />
+            <Button buttonStyle="number-btn" text="2" />
+            <Button buttonStyle="number-btn" text="3" />
+            <Button buttonStyle="function-btn" text="+" />
+            <Button buttonStyle="action-btn" text="0" />
+            <Button buttonStyle="function-btn" text="=" />
 
 
         </div>
